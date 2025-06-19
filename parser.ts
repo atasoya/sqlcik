c79865f68112b4079b82228f323b71e5f44bf6bc
atasoya <ataatasoy2013@gmail.com>
2025-06-19 21:48:55 +0300

    simple parser

--- parser.ts
+++ parser.ts
@@ -1,7 +1,15 @@
 // i will skip creating table for now 
 
-export const Command = {name:"insert", id:number, name:string }
+import { warn } from "console"
 
-export function parseCommand():Command {
+// example promt: insert 0 ata atasoy 22
+type Command = {commandType: String, id: String, name: String, surname: String, age: String}
+
+export function parseCommand(promt: String): Command  {
     // handle parser here
+    let promtArguments = promt.split("\\s+")
+    let userCommand: Command = {commandType:promtArguments[0] ,id:promtArguments[1], name: promtArguments[2], surname: promtArguments[2],
+        age: promtArguments[2]
+    }
+    return userCommand
 }
