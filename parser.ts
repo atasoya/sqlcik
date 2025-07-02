be1f31e63e76db1309dc016399ad72a33e01937e
atasoya <ataatasoy2013@gmail.com>
2025-07-02 13:45:13 +0300

    use shared types

--- parser.ts
+++ parser.ts
@@ -1,14 +1,16 @@
 // i will skip creating table for now 
 // example promt: insert 0 ata atasoy 22
-type Command = {commandType: String, id: String, name: String, surname: String, age: String}
+import { User, Command } from "./types"
 
 export function parseCommand(promt: String): Command  {
     // handle parser here
     console.log(promt)
     let promtArguments = promt.split(/(\s+)/).filter( e => e.trim().length > 0)
     console.log(promtArguments)
-    let userCommand: Command = {commandType:promtArguments[0] ,id:promtArguments[1], name: promtArguments[2], surname: promtArguments[3],
-        age: promtArguments[4]
-    }
+    let commandUser:User = {id:promtArguments[1], name: promtArguments[2], surname: promtArguments[3],
+        age: promtArguments[4]}
+{
+    let userCommand: Command = {commandType:promtArguments[0],user:commandUser}
     return userCommand
+}
 }
